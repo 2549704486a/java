ab9f2913c00e4f7d231e3bd2a60b09350cb4cf47
2549704486a <2549704486@qq.com>
2024-10-17 10:54:12 +0800

    改动见开发日志11~16

---
+++ incentive/src/main/java/com/budou/incentive/utils/SentinelUtil.java
@@ -0,0 +1,11 @@
+package com.budou.incentive.utils;/**
+* @program: incentive-事务消息
+*
+* @description: 
+*
+* @author: 阿伟
+*
+* @create: 2024-10-26 20:18
+**/
+public class SentinelUtil {
+}
---
+++ incentive/src/test/java/com/budou/incentive/RocketMQTemplateTest.java
@@ -0,0 +1,11 @@
+package com.budou.incentive;/**
+* @program: incentive-事务消息
+*
+* @description: 
+*
+* @author: 阿伟
+*
+* @create: 2024-10-27 14:46
+**/
+public class RocketMQTemplateTest {
+}
